96360b2470431232bb19dfbb78fe39162a5dfdfc
jwolf503 <wolfanger.jason1@gmail.com>
2026-01-06 13:59:44 -0500

    firtsucces

--- sh.nix
+++ sh.nix
@@ -6,6 +6,7 @@ let
       ".." = "cd ..";
       vim = "vim -S ~/.vim/vimrc";
       gvim = "gvim -S ~/.vim/vimrc";
+      g = "gvim -S ~/.vim/vimrc";
       ncf = "cd ~/nixos-dotfiles";
       nhs = "home-manager switch --flake ~/nixos-dotfiles";
       nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos-jay";
@@ -22,6 +23,16 @@ in
       export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
       nitch
       export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow'
+      eval "$(zoxide init bash)"
     '';
+};
+   programs.fish = {
+       enable = true;
+       shellAliases = myAliases;
+
+       interactiveShellInit = ''
+           zoxide init fish | source
+           nitch
+       '';
   };
 }
