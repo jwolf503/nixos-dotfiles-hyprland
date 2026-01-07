3b6f15bd6f2145c9c7213720cb9c995277ba0f4b
jwolf503 <wolfanger.jason1@gmail.com>
2026-01-06 21:23:08 -0500

    add brave policy

---
+++ brave-policy/default.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.systemSettings.brave;
+in {
+  options = {
+    systemSettings.brave = {
+      enable = lib.mkEnableOption "Enable brave group policies";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.etc."/brave/policies/managed/GroupPolicy.json".text = ''
+      {
+       "BrowserSignin": 0,
+        "PasswordManagerEnabled": false,
+        "SpellcheckEnabled": true,
+        "SpellcheckLanguage": [
+          "en-US"
+        ],
+        "TorDisabled": true,
+        "BraveRewardsDisabled": true,
+        "BraveWalletDisabled": true,
+        "BraveVPNDisabled": true,
+        "BraveAIChatEnabled": false,
+        "BraveNewsDisabled": true,
+        "BraveTalkDisabled": true,
+        "BraveSpeedreaderEnabled": false,
+        "BraveP3AEnabled": false,
+        "BraveStatsPingEnabled": false,
+        "BraveWebDiscoveryEnabled": false,
+      }
+    '';
+  };
+}
